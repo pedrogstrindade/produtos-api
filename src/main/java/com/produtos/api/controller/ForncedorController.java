ba4d842d96b785125eb898416055f9607ca8e0a1
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-15 21:45:17 -0300

    Endpoint PutMapping para atualizar dados do fornecedor pelo id

--- src/main/java/com/produtos/api/controller/ForncedorController.java
+++ src/main/java/com/produtos/api/controller/ForncedorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +57,16 @@ public class ForncedorController {
 
         fornecedorService.deletarFornecedor(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fornecedor> atualizarFornecedor(@PathVariable("id") Long id, @RequestBody Fornecedor dadosFornecedor) {
+        Fornecedor fornecedor = fornecedorService.acharFornecedorPeloId(id);
+
+        if (Objects.isNull(fornecedor)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        return ResponseEntity.ok().body(fornecedorService.atualizarFornecedor(id, dadosFornecedor));
     }
 }
