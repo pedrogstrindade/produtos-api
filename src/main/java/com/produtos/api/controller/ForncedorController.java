954ab36b84923ea08480d245c3ac990382cf67a5
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-15 21:42:00 -0300

    Endpoint GetMapping para listar fornecedores (paginação)

--- src/main/java/com/produtos/api/controller/ForncedorController.java
+++ src/main/java/com/produtos/api/controller/ForncedorController.java
@@ -1,8 +1,11 @@
 package com.produtos.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class ForncedorController {
     @PostMapping
     public ResponseEntity<Fornecedor> cadastrarFornecedor(@RequestBody Fornecedor fornecedor) {
         return ResponseEntity.status(HttpStatus.CREATED).body(fornecedorService.cadastrarFornecedor(fornecedor));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Fornecedor>> listarFornecedores(Pageable paginacao) {
+        return ResponseEntity.ok().body(fornecedorService.listarFornecedores(paginacao));
     }
 }
