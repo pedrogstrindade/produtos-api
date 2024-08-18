0dc6e1068bba7ee96043e1a0bbead224373355e3
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-18 14:12:00 -0300

    GetMapping achar usuarios pelo id

--- src/main/java/com/produtos/api/controller/UsuarioController.java
+++ src/main/java/com/produtos/api/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.produtos.api.controller;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,16 @@ public class UsuarioController {
     @GetMapping
     public ResponseEntity<Page<UsuarioDTO>> listarUsuarios(Pageable paginacao) {
         return ResponseEntity.ok().body(usuarioService.listarUsuarios(paginacao));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> acharUsuarioPeloId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.acharUsuarioPeloId(id);
+
+        if (Objects.isNull(usuario)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(usuario);
     }
 }
