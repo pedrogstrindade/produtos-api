9e3ceb80988f016ccfebcd3cd40974a8478d6349
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-18 14:10:54 -0300

    Listagem de todos usuarios

--- src/main/java/com/produtos/api/controller/UsuarioController.java
+++ src/main/java/com/produtos/api/controller/UsuarioController.java
@@ -1,13 +1,17 @@
 package com.produtos.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.produtos.api.dto.UsuarioDTO;
 import com.produtos.api.model.Usuario;
 import com.produtos.api.service.UsuarioService;
 
@@ -21,5 +25,10 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioDTO>> listarUsuarios(Pageable paginacao) {
+        return ResponseEntity.ok().body(usuarioService.listarUsuarios(paginacao));
     }
 }
