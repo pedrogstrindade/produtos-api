973143f581cdc65587e3eb97aeb6db1a144207b9
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-18 13:45:12 -0300

    Repositório de usuário com 3 métodos adicionais

---
+++ src/main/java/com/produtos/api/repository/UsuarioRepository.java
@@ -0,0 +1,21 @@
+package com.produtos.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.produtos.api.constants.UsuarioStatus;
+import com.produtos.api.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+    public Page<Usuario> findByNomeLike(String nome, Pageable paginacao);
+
+    public Optional<Usuario> findByCpf(String cpf);
+
+    public Page<Usuario> findByStatus(UsuarioStatus status);
+}
