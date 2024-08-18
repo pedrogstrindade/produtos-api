06bf0f88813225ef6414c22b69b78e83e81da843
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-18 14:12:29 -0300

    GetMapping achar usuario pelo cpf

--- src/main/java/com/produtos/api/controller/UsuarioController.java
+++ src/main/java/com/produtos/api/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.produtos.api.dto.UsuarioDTO;
@@ -38,6 +39,17 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioDTO> acharUsuarioPeloId(@PathVariable Long id) {
         UsuarioDTO usuario = usuarioService.acharUsuarioPeloId(id);
+
+        if (Objects.isNull(usuario)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(usuario);
+    }
+
+    @GetMapping("/cpf")
+    public ResponseEntity<UsuarioDTO> acharUsuarioPeloCpf(@RequestParam String cpf) {
+        UsuarioDTO usuario = usuarioService.acharUsuarioPeloCpf(cpf);
 
         if (Objects.isNull(usuario)) {
             return ResponseEntity.notFound().build();
