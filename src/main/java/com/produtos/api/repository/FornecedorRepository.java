8cd493b8a6a92205c12dbea6f7d6b6c4cd912f51
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-19 19:52:50 -0300

    Método de busca de fornecedor pelo cnpj

--- src/main/java/com/produtos/api/repository/FornecedorRepository.java
+++ src/main/java/com/produtos/api/repository/FornecedorRepository.java
@@ -1,5 +1,7 @@
 package com.produtos.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.produtos.api.model.Fornecedor;
 
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+
+    Optional<Fornecedor> findByCnpj(String cnpj);
 }
