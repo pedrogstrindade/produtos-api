18732bf7b7891f2999fbaea83c58e7a212fceffd
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-19 19:56:05 -0300

    Método de busca de fornecedor pelo nome

--- src/main/java/com/produtos/api/repository/FornecedorRepository.java
+++ src/main/java/com/produtos/api/repository/FornecedorRepository.java
@@ -2,6 +2,8 @@ package com.produtos.api.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ import com.produtos.api.model.Fornecedor;
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
     Optional<Fornecedor> findByCnpj(String cnpj);
+    
+    Page<Fornecedor> findByNomeContains(String nome, Pageable paginacao);
 }
