d5ae517e0d80f6f445b573580da69c9b9b8c00a0
Pedro Trindade <ppeedrotrindade82@gmail.com>
2024-08-19 12:08:22 -0300

    DeleteMapping por id

--- src/main/java/com/produtos/api/controller/UsuarioController.java
+++ src/main/java/com/produtos/api/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,17 @@ public class UsuarioController {
     @GetMapping
     public ResponseEntity<Page<UsuarioDTO>> listarUsuariosPorStatus(@RequestParam(name = "status", defaultValue = "ATIVO") UsuarioStatus status, Pageable paginacao) {
         return ResponseEntity.ok().body(usuarioService.listarUsuariosPeloStatus(status, paginacao));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.acharUsuarioPeloId(id);
+
+        if (Objects.isNull(usuario)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
     }
 }
